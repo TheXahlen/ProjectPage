a5437deb36cc7aeaedb272edeba894439c1c52dd
Dahlen Bryce <47333926+TheXahlen@users.noreply.github.com>
2021-09-25 00:27:06 -0700

    Update adventure.js

--- scripts/adventure.js
+++ scripts/adventure.js
@@ -1,1 +1,31 @@
-
+let Location = "Center"
+document.addEventListener('keydown', (event)=> {    
+ if (event.key == "ArrowDown"){
+ 		console.log("Down Arrow!")
+    if (Location = "Center"){
+    Location = "South"
+    }
+    if (Location = "North"){
+    Location = "Center"
+    }
+    
+ }
+  if (event.key == "ArrowUp"){
+ 		console.log("Up Arrow!")
+    if (Location = "Center"){
+    Location = "North"
+    }
+ }
+  if (event.key == "ArrowLeft"){
+ if (Location = "Center"){
+    Location = "West"
+    }
+ }
+  if (event.key == "ArrowRight"){
+ 		console.log("Right Arrow!")
+    if (Location = "Center"){
+    Location = "East"
+    }
+    ""
+ }}
+});
